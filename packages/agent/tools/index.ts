48941b30105617f8a5ede86a01a9ee412f8a0beb
Second chance <miraclethirdbase1@gmail.com>
2026-08-14 11:12:06 +0100

    Export unified githubCliTool

--- packages/agent/tools/index.ts
+++ packages/agent/tools/index.ts
@@ -17,10 +17,6 @@ export {
 } from "./ask-user-question";
 export { skillTool, type SkillToolInput } from "./skill";
 export { webFetchTool } from "./fetch";
-export {
-  githubCommitTool,
-  githubPrCommentsTool,
-  type GithubCommitToolInput,
-} from "./github";
+export { githubCliTool, type GithubCliToolInput } from "./github";
 export { vercelCliTool, type VercelCliToolInput } from "./vercel";
 export { webSearchTool } from "./web-search";
